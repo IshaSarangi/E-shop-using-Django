01d71d3d6adef17b8b9781997a4e4a8feede9cd3
IshaSarangi <ishasarangi@gmail.com>
2022-05-11 23:09:13 +0530

    commit changes

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/IshaWorksblahblah/blah/blah/blah/"
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models.product import Product
+from .models.category import Category
 
 # Create your views here.
+# def index(request):
+#     return HttpResponse('<H1> Index Page </H1>')
+
+def index(request):
+    products = Product.get_all_products();
+    categories = Category.get_all_categories();
+    # print(products)
+    # return render(request, 'orders/order.html')
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
