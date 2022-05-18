ded9e7b8d0f2c7490412f155b88ea6c452dc08b0
IshaSarangi <ishasarangi@gmail.com>
2022-05-18 09:34:57 +0530

    Cart

--- store/Views/home.py
+++ store/Views/home.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from store.models.product import Product
 from store.models.category import Category
+from django.views import View
 
 # print(make_password('1234'))
 # print(check_password('1234','pbkdf2_sha256$320000$BXfmD5ytn1Fb37zTnrcBCV$OOOtkKATefBN22pXi6m9iVnw6G236Vt17vurE2gXJqg='))
@@ -9,20 +10,53 @@ from store.models.category import Category
 # def index(request):
 #     return HttpResponse('<H1> Index Page </H1>')
 
-def index(request):
-    products = None
-    categories = Category.get_all_categories();
-    # print(products)
-    # return render(request, 'orders/order.html')
-    categoryID = (request.GET.get('category'))
-    if categoryID:
-        products = Product.get_all_products_by_categoryid(categoryID)
-    else:
-        products = Product.get_all_products();
-    data = {}
-    data['products'] = products
-    data['categories'] = categories
-    print('You are', request.session.get('email'))
+class Index(View):
     
-    return render(request, 'index.html', data)
+    def post(self, request):
+        product = request.POST.get("product")
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:   
+                if remove:   
+                    if quantity <= 1:
+                        cart.pop(product)
+                    else:
+                        cart[product] = quantity-1
+                else:
+                    cart[product] = quantity+1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        
+        request.session['cart'] = cart
+        print("Cart:", request.session['cart'])
+        return redirect('homepage')
+    
+    def get(self, request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session['cart'] = {}
+            
+        
+        products = None
+        categories = Category.get_all_categories();
+        # print(products)
+        # return render(request, 'orders/order.html')
+        categoryID = (request.GET.get('category'))
+        if categoryID:
+            products = Product.get_all_products_by_categoryid(categoryID)
+        else:
+            products = Product.get_all_products();
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
+        print('You are', request.session.get('email'))
+    
+        return render(request, 'index.html', data)
+
+
    
--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .Views import home, login, signup
-# from .Views.home import index
-# from .Views.signup import Signup
-# from .Views.login import Login
+# from .Views import home, login, signup
+from .Views.home import Index
+from .Views.signup import Signup
+from .Views.login import Login
 
 urlpatterns = [
-    path('', home.index, name='homepage'),
-    path('signup', signup.Signup.as_view(), name='signup'),
-    path('login', login.Login.as_view(), name='login')
+    path('', Index.as_view(), name='homepage'),
+    path('signup', Signup.as_view(), name='signup'),
+    path('login', Login.as_view(), name='login')
 ]
