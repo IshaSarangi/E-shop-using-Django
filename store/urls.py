27fefdedcb9002c11766a23f694101e4617d1bfe
IshaSarangi <ishasarangi@gmail.com>
2022-05-19 09:36:55 +0530

    logout

--- store/urls.py
+++ store/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 # from .Views import home, login, signup
 from .Views.home import Index
 from .Views.signup import Signup
-from .Views.login import Login
+from .Views.login import Login, logout
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('signup', Signup.as_view(), name='signup'),
-    path('login', Login.as_view(), name='login')
+    path('login', Login.as_view(), name='login'),
+    path('logout', logout, name='logout')
 ]
